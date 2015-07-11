cf8c6d9b797a8293108f580c3129b5d5b35dfa98
Florian Scherf <fscherf@gmx.net>
2015-07-11 04:14:30 +0200

    urls: add urlpatterns for screens
    Signed-off-by: Florian Scherf <fscherf@gmx.net>

--- info_display/info_display/urls.py
+++ info_display/info_display/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+screen_urlpatterns = [
+    url(r'^bus/', include('bus.urls', namespace='bus', app_name='bus')),
+]
+
 urlpatterns = [
+    url(r'^screens/', include(screen_urlpatterns)),
     url(r'^main-screen/$', 'info_display.views.main_screen',
         name='main_screen'),
-    url(r'^bus/', include('bus.urls', namespace='bus', app_name='bus')),
     url(r'', include(admin.site.urls)),
 ]
