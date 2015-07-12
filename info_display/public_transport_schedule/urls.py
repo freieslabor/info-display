c927780890083dd0942a7e6c6cc0a70eae6d8c02
Florian Scherf <fscherf@gmx.net>
2015-07-12 02:23:08 +0200

    public_transport_schedule: urls: add names for reverse resolving
    Signed-off-by: Florian Scherf <fscherf@gmx.net>

--- info_display/public_transport_schedule/urls.py
+++ info_display/public_transport_schedule/urls.py
@@ -3,6 +3,8 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    url(r'^schedule.json$', 'public_transport_schedule.views.schedule_json'),
-    url(r'^$', TemplateView.as_view(template_name='schedule.html')),
+    url(r'^schedule.json$', 'public_transport_schedule.views.schedule_json',
+        name='schedule_data'),
+    url(r'^$', TemplateView.as_view(template_name='schedule.html'),
+        name='schedule'),
 ]
