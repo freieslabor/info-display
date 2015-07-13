56277c492afba8089d2462020d7a40fa9ec45061
Florian Scherf <fscherf@gmx.net>
2015-07-13 03:14:00 +0200

    main_screen: api: refresh screen before showing it
    Signed-off-by: Florian Scherf <fscherf@gmx.net>

--- info_display/info_display/static/info_display/info-display-api.js
+++ info_display/info_display/static/info_display/info-display-api.js
@@ -1,39 +1,52 @@
+var screens = $('iframe.screen');
+
 function get_info() {
-    var screens = $('iframe.screen');
-    return new Array($(screens).length, $(screens).index($('iframe.screen.active')));
+    return new Array(screens.length, screens.index($('iframe.screen.active')));
 }
 
 function show_next_screen() {
-    var screens = $('iframe.screen');
-    var active_screen = $(screens).index($('iframe.screen.active'));
+    var next_screen_index = screens.index($('iframe.screen.active')) + 1;
+
+    if(next_screen_index >= screens.length) {
+        next_screen_index = 0;
+    }
+
+    try {
+        $(screens)[next_screen_index].contentWindow.refresh();
+    } catch(error) {
+        /* nah... nevermind! */
+    }
 
     $(screens).removeClass('active');
-
-    if(active_screen + 1 >= $(screens).length) {
-        $(screens[0]).addClass('active');
-    } else {
-        $(screens[active_screen + 1]).addClass('active');
-    }
+    $(screens[next_screen_index]).addClass('active');
 }
 
 function show_previous_screen() {
-    var screens = $('iframe.screen');
-    var active_screen = $(screens).index($('iframe.screen.active'));
+    var previous_screen_index = screens.index($('iframe.screen.active')) - 1;
+
+    if(previous_screen_index < 0) {
+        previous_screen_index = screens.length - 1;
+    }
+
+    try {
+        $(screens)[previous_screen_index].contentWindow.refresh();
+    } catch(error) {
+        /* nah... nevermind! */
+    }
 
     $(screens).removeClass('active');
-
-    if(active_screen - 1 >= 0) {
-        $(screens[active_screen - 1]).addClass('active');
-    } else {
-        $(screens[$(screens).length - 1]).addClass('active');
-    }
+    $(screens[previous_screen_index]).addClass('active');
 }
 
 function show_screen(index) {
-    var screens = $('iframe.screen');
-
-    if(index < 0 || index > $(screens).length - 1) {
+    if(index < 0 || index > screens.length - 1) {
         return false;
+    }
+
+    try {
+        $(screens)[index].contentWindow.refresh();
+    } catch(error) {
+        /* nah... nevermind! */
     }
 
     $(screens).removeClass('active');
